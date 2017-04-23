d2081473905b61aa3df019b256dbcc405e0e6de1
Igor Janssen <ijanssen@ics.com>
2017-04-24 04:20:13 +0700

    Add documentation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # qmlbattery
 QML battery indicator
+
+Compilation.
+Use Qt 5.8 or higher.
+Edit qmlbattery.pri for backend select:
+Use next for emulation backend
+DEFINES += EMUL
+Use next for linux UPower dbus backend
+DEFINES += LINUX
--- src/batteryinfo.h
+++ src/batteryinfo.h
@@ -4,15 +4,42 @@
 #include <QObject>
 
 class BatteryInfoPrivate;
-
+/*!
+ * \brief Battery information class
+ */
 class BatteryInfo : public QObject
 {
     Q_OBJECT
 
+    /*!
+     * \brief Battery status
+     *  StatusFull - battery charged
+     *  StatusCharging - battery is charging
+     *  StatusDischarging - battery is discharging
+     *  StatusMissing - there is no battery
+     */
     Q_PROPERTY(Status status READ status NOTIFY statusChanged)
+    /*!
+     * \brief Battery charge level
+     *  LevelEmpty     0% charge
+     *  LevelCritical  <= 15%
+     *  LevelLow       <= 40%
+     *  LevelMedium    <= 70%
+     *  LevelHigh      < 100%
+     *  LevelFull   full charge, 100%
+     */
     Q_PROPERTY(Level level READ level NOTIFY levelChanged)
+    /*!
+     * \brief percentage of charge level
+     */
     Q_PROPERTY(double percentage READ percentage NOTIFY percentageChanged)
+    /*!
+     * \brief Time in seconds for 100% charge
+     */
     Q_PROPERTY(quint64 timeToFull READ timeToFull NOTIFY timeToFullChanged)
+    /*!
+     * \brief Time in seconds for full discharged
+     */
     Q_PROPERTY(quint64 timeToEmpty READ timeToEmpty NOTIFY timeToEmptyChanged)
 
     Q_ENUMS(Status)
